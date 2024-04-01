0b978c10c3c142351c8a10195a2797b31f48510b
unknown <sanchitdeshmukh23@gmail.com>
2024-04-01 13:56:45 +0530

    project completed

--- nike-brandpage/src/components/Brandpage.jsx
+++ nike-brandpage/src/components/Brandpage.jsx
@@ -19,6 +19,28 @@ function Brandpage() {
           <a href="">Login</a>
         </nav>
 
+        <div id='container'>
+
+          <div id="container-1">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p id='description'>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div id="box-1">
+              <a href="" className='btn'>Shop Now</a>
+              <a href="" className='btn'>Category</a>
+            </div>
+            <p>Also available on</p>
+            <div id="box-2">
+              <img src="/images/flipkart.png" alt="" />
+              <img src="/images/amazon.png" alt="" />
+            </div>
+          </div>
+
+          <div id="container-2">
+            <img src="/images/shoe_image.png" alt="shoes" />
+          </div>
+
+        </div>
+
       </div>
     </>
   )
